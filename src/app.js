4cfb54f5f3d7a8db73f5bcad983d42c23454e150
romain-next <romain.mourieras@next-u.fr>
2026-03-03 15:58:27 +0100

    feat(crud): create base route and db connection

---
+++ src/app.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const { connectDB, sequelizeInstance } = require("./db")
+const app = express()
+
+app.use(express.json());
+
+const user = require("./model/user")
+
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Mon API DevOps' })
+})
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' })
+})
+
+app.get('/users', async (req, res) => {
+  const users = await user.findAll();
+  res.status(200).json({ status: 'ok', data:users })
+})
+
+app.get('/users/:id', async (req, res) => {
+  const userId = req.params.id
+  const oneUser = await user.findOne({where: {id: userId}});
+  if(!oneUser){
+    return res.status(404).json({status:"not found"})
+  }
+  res.status(200).json({ status: 'ok', data:oneUser })
+})
+
+app.post('/users', async (req, res) => {
+  const body = req.body
+  if(!body.name || !body.firstName || !body.email){
+    return res.status(400).json({status: "error", message:"Champ manquant"})
+  }
+  const {firstName, email, name} = body
+  const userCreated = await user.create({firstName,email,name });
+  res.status(201).json({ status: 'ok', data:userCreated })
+})
+
+app.put('/users/:id', async (req, res) => {
+  const userId = req.params.id
+  const oneUser = await user.findOne({where: {id: userId}});
+  if(!oneUser){
+    return res.status(404).json({status:"not found"})
+  }
+  const body = req.body
+  if(!body){
+    return res.status(400).json({status:"error"})
+  }
+  const userUpdated = await oneUser.update(body);
+  res.status(200).json({ status: 'ok', data:userUpdated })
+})
+
+app.delete('/users/:id', async (req, res) => {
+  const userId = req.params.id
+  const oneUser = await user.findOne({where: {id: userId}});
+  if(!oneUser){
+    return res.status(404).json({status:"not found"})
+  }
+  const userDeleted = await oneUser.destroy();
+  res.status(200).json({ status: 'ok', data:userDeleted })
+})
+
+connectDB().then(() => {
+  sequelizeInstance.sync();
+  app.listen(PORT, () =>
+    console.log(`Serveur démarré sur http://localhost:${PORT}`),
+  );
+});
+
+
+module.exports = app
---
+++ src/db.js
@@ -0,0 +1,25 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config()
+
+const sequelizeInstance = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+  {
+    host:  process.env.DB_HOST,
+    dialect:  process.env.DB_DIALECT,
+    port:  process.env.DB_PORT,
+  },
+);
+
+async function connectDB() {
+  try {
+    await sequelizeInstance.authenticate();
+    console.log('✅ Connexion DB réussie');
+  } catch (error) {
+    console.error('❌ Erreur de connexion DB:', error);
+    process.exit(1);
+  }
+}
+
+module.exports = {sequelizeInstance, connectDB}
